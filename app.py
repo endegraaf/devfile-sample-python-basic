055827b79fc162f7f4cb9e0c8ec884d715b2a7e5
Eric de Graaf <endegraaf@gmail.com>
2022-11-24 10:15:43 +0100

    add new route

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/endegraaf')
+def endegraaf():
+    return "Hello You!"
+
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
